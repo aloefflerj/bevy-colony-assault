af9477e1bec8dfe2165acea655deb7a5213ff8a4
Anderson Job Loeffler <aloeffler.j@gmail.com>
2024-09-15 16:49:12 -0300

    :sparkles: feat: adds bg color

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,16 @@ use player::PlayerPlugin;
 
 fn main() {
     let default_plugin_options = DefaultPluginOptions::new();
+    let bg_color = Srgba::hex("14081a").unwrap();
+    let bg_color = Color::srgba(bg_color.red, bg_color.green, bg_color.blue, bg_color.alpha);
+
     App::new()
         .add_plugins(
             DefaultPlugins
                 .set(default_plugin_options.image_plugin)
                 .set(default_plugin_options.window_plugin),
         )
+        .insert_resource(ClearColor(bg_color))
         .add_plugins(CameraPlugin)
         .add_plugins(PlatformPlugin)
         .add_plugins(PlayerPlugin)
