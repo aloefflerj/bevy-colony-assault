d34d5041dcd8ec06b78f94237b89356f96e016df
Anderson Job Loeffler <aloeffler.j@gmail.com>
2024-09-16 20:09:25 -0300

    :bug: fix: applies force before handle force on player physics

--- src/player/physics/mod.rs
+++ src/player/physics/mod.rs
@@ -10,6 +10,12 @@ const PLAYER_X_SPEED: f32 = 150.;
 const PLAYER_JUMP_SPEED: f32 = 250.;
 const PLAYER_MAX_JUMP_HEIGHT: f32 = 1.;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct HandleForce;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ApplyForce;
+
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum PlayerGroundState {
     #[default]
@@ -24,20 +30,27 @@ impl Plugin for PlayerPhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<PlayerGroundState>();
         app.init_resource::<PlayerJumpProperties>();
+        app.configure_sets(Update, HandleForce.before(ApplyForce));
         app.add_systems(
             Update,
             (
-                move_x,
-                jump.run_if(in_state(PlayerGroundState::Grounded)),
-                rise.run_if(in_state(PlayerGroundState::Rising)),
-                apply_gravity.run_if(
-                    in_state(PlayerGroundState::Falling)
-                        .or_else(in_state(PlayerGroundState::Rising)),
-                ),
-                handle_ground_touch.run_if(
-                    in_state(PlayerGroundState::Falling)
-                        .or_else(in_state(PlayerGroundState::Grounded)),
-                ),
+                move_x.in_set(ApplyForce),
+                jump.run_if(in_state(PlayerGroundState::Grounded))
+                    .in_set(ApplyForce),
+                rise.run_if(in_state(PlayerGroundState::Rising))
+                    .in_set(ApplyForce),
+                apply_gravity
+                    .run_if(
+                        in_state(PlayerGroundState::Falling)
+                            .or_else(in_state(PlayerGroundState::Rising)),
+                    )
+                    .in_set(HandleForce),
+                handle_ground_touch
+                    .run_if(
+                        in_state(PlayerGroundState::Falling)
+                            .or_else(in_state(PlayerGroundState::Grounded)),
+                    )
+                    .in_set(HandleForce),
             ),
         );
     }
